1ca2ca755afcd1d2b79eee9352a8a6c836eccc4f
Anolla <annehaddad96@yahoo.com>
2020-04-04 16:20:16 +0300

    ch4

--- code-challenges/Regular Expressionsp1/challenges-04.test.js
+++ code-challenges/Regular Expressionsp1/challenges-04.test.js
@@ -28,6 +28,9 @@ Return an array containing all the matches.
 
 const isCapitalized = (str) => {
     // Solution code here...
+    var matches = /\b[A-Z].*?\b/g
+    let arr = []
+    return str.match(matches) || arr
 };
 
 /* ------------------------------------------------------------------------------------------------
